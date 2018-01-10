bf3a3149d864a1860e2614136e82dcf44fcdd7e6
EdLeafe <ed@leafe.com>
2018-01-10 13:35:06 -0600

    Added PowerBall support

--- lotto/cmd/qpick/main.go
+++ lotto/cmd/qpick/main.go
@@ -2,16 +2,33 @@ package main
 
 import (
 	"fmt"
-	"github.com/EdLeafe/lotto/lib/mega"
+	"github.com/EdLeafe/lotto/lib/games"
+    "os"
+    "strings"
 )
 
 func main() {
-	res := mega.QuickPick()
+    // Default to mega millions
+    game := "mega"
+    range1 := 75
+    range2 := 25
+    ballname := " Mega"
+    args := os.Args[1:]
+    if len(args) > 0 {
+        game = strings.ToLower(args[0])
+    }
+    if strings.HasPrefix(game, "pow") {
+        range1 = 69
+        range2 = 26
+        ballname = "Power"
+    }
+
+	res := games.QuickPick(range1, range2)
 	picks := []int{res.P0, res.P1, res.P2, res.P3, res.P4}
 	fmt.Printf("Your numbers are: ")
 	for _, num := range picks {
 		fmt.Printf("%d ", num)
 	}
 	fmt.Printf("\n")
-	fmt.Printf("            Mega: %d\n", res.Mega)
+	fmt.Printf("           %s: %d\n", ballname, res.Ball)
 }
--- spamcheck/splitter.go
+++ spamcheck/splitter.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"io"
 	"net/mail"
-//	"net/smtp"
+	//	"net/smtp"
 	"os"
 	"regexp"
 	"sort"
@@ -48,49 +48,61 @@ func check(e error) {
 	}
 }
 
-func updateSubject(h mail.Header, smap, emap map[string]int) {
+func updateSubject(h mail.Header) []string {
 	edexp := regexp.MustCompile(EDPAT)
 	sub := h.Get("Subject")
-	smap[sub] += 1
+	ret := []string{sub, ""}
 	if edexp.MatchString(sub) {
-		emap[sub] += 1
+		ret[1] = sub
 	}
+	return ret
 }
 
-func updateFrom(h mail.Header, fmap map[string]int) {
+func updateFrom(h mail.Header) string {
 	sendexp := regexp.MustCompile(SENDPAT)
 	from := h.Get("From")
 	match := sendexp.FindStringSubmatch(from)
 	if len(match) > 0 {
 		from = match[len(match)-1]
-		fmap[from] += 1
+		//fmap[from] += 1
+		return from
 	}
+	return ""
 }
 
-func updateRecip(h mail.Header, rmap map[string]int) {
+func updateRecip(h mail.Header) []string {
 	orig := h.Get("X-Original-To")
+	ret := []string{"", ""}
 	if len(orig) > 0 {
-		rmap[orig] += 1
-		return
+		//rmap[orig] += 1
+		ret[0] = orig
 	}
 	toAddr := h.Get("To")
 	if len(toAddr) > 0 {
-		rmap[toAddr] += 1
+		//	rmap[toAddr] += 1
+		ret[1] = toAddr
 	}
+	return ret
 }
 
-func processMsg(wg *sync.WaitGroup, txt string, subjs, eds, senders,
-        recips map[string]int) {
-    defer wg.Done()
+func processMsg(wg *sync.WaitGroup, txt string, chSubj chan string,
+	chEds chan string, chSend chan string, chRecp chan string) {
+
+	defer wg.Done()
 	stxt := strings.TrimSpace(txt)
 	r := strings.NewReader(stxt)
 	msg, err := mail.ReadMessage(r)
 	check(err)
 	h := msg.Header
 
-	updateSubject(h, subjs, eds)
-	updateFrom(h, senders)
-	updateRecip(h, recips)
+	ss := updateSubject(h)
+	chSubj <- ss[0]
+	chEds <- ss[1]
+	f := updateFrom(h)
+	chSend <- f
+	rr := updateRecip(h)
+	chSend <- rr[0]
+	chRecp <- rr[1]
 }
 
 func sortStringCount(txts map[string]int, ones bool) []string {
@@ -121,6 +133,10 @@ func main() {
 
 	var wg sync.WaitGroup
 	var buf bytes.Buffer
+	chSubj := make(chan string, 10000)
+	chEds := make(chan string, 10000)
+	chSend := make(chan string, 10000)
+	chRecp := make(chan string, 10000)
 	splitter := regexp.MustCompile(MSGSPLIT)
 	f, err := os.Open(FNAME)
 	check(err)
@@ -144,24 +160,42 @@ func main() {
 				pos := start + 1
 				txt := alltext[:pos]
 				buf.WriteString(alltext[pos:])
-                wg.Add(1)
-				go processMsg(&wg, txt, subjs, eds, senders, recips)
+				wg.Add(1)
+				go processMsg(&wg, txt, chSubj, chEds, chSend, chRecp)
 			}
 		}
 	}
 	// We've gotten to the end, so the buffer will contain the last message.
 	// Process that one, and we're done.
-	go processMsg(&wg, buf.String(), subjs, eds, senders, recips)
-    wg.Wait()
+	wg.Add(1)
+	go processMsg(&wg, buf.String(), chSubj, chEds, chSend, chRecp)
+	wg.Wait()
+    close(chSubj)
+    close(chEds)
+    close(chSend)
+    close(chRecp)
+	for ss := range chSubj {
+		subjs[ss] += 1
+	}
+	for ss := range chEds {
+		eds[ss] += 1
+	}
+	for ss := range chSend {
+		senders[ss] += 1
+	}
+	for ss := range chRecp {
+		recips[ss] += 1
+	}
 
 	sortedSubjects := sortStringCount(subjs, false)
 	sortedRecips := sortStringCount(recips, true)
 	sortedEds := sortStringCount(eds, false)
+	sortedSenders := sortStringCount(senders, false)
 
-	if sortedSubjects == nil || sortedRecips == nil {
+	if sortedSubjects == nil || sortedEds == nil || sortedSenders == nil || sortedRecips == nil{
 		fmt.Println("Just to keep the compiler from complaining.")
 	}
-	for _, itm := range sortedEds {
+	for _, itm := range sortedSubjectsdasdasdas{
 		fmt.Println(itm)
 	}
 }
